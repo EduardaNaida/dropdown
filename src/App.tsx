8023f1554df8f99a809d3e69e59fc704fd409e90
EduardaNaida <eduarda.naida@gmail.com>
2023-03-25 13:24:58 +0300

    feat: add checkbox and selectItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-import {MultiSelect} from "./components/MultiSelect";
+import {MultiSelect, MultiSelectType} from "./components/MultiSelect";
+
+const state = [
+  {id: 0, label: 'English'},
+  {id: 1, label: 'German'},
+  {id: 2, label: 'Spanish'},
+  {id: 3, label: 'Polish'},
+  {id: 4, label: 'Polish'},
+]
 
 function App() {
+
+  const [value, onChangeOption] = useState<MultiSelectType[]>([state[0]])
+
   return (
     <div className="App">
-      <MultiSelect/>
+      <MultiSelect options={state} value={value} onChangeOption={e => onChangeOption(e)}/>
     </div>
   );
 }
--- src/components/MultiSelect.tsx
+++ src/components/MultiSelect.tsx
@@ -1,37 +1,92 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, DetailedHTMLProps, SelectHTMLAttributes, useState} from 'react';
 import style from './MultiSelect.module.css'
 import flag from '../image/germanFlag.svg'
 
-export const MultiSelect = () => {
+export type MultiSelectType = {
+  id: number,
+  label: string
+}
+
+type SuperSelectPropsType = {
+  value: MultiSelectType[]
+  options: any[]
+  onChangeOption: (option: MultiSelectType[]) => void
+}
+
+export const MultiSelect: React.FC<SuperSelectPropsType> = ({options, onChangeOption, value}) => {
 
   const [isOpen, setIsOpen] = useState<boolean>(false)
 
-  const state = [
-    {value: 'english', label: 'English'},
-    {value: 'german', label: 'German'},
-    {value: 'spanish', label: 'Spanish'},
-    {value: 'polish', label: 'Polish'},
-  ]
+  const [selectedItem, setSelectedItem] = useState<MultiSelectType[]>([options[0]])
+
+  const removeItem = (id: number) => {
+    const option = value.filter(option => option.id != id)
+    onChangeOption([...option])
+  }
+
+  const selectItem = (option: MultiSelectType) => {
+    // if (selectedItem.includes(value)) {
+    //   setSelectedItem(selectedItem.filter(option => option !== value))
+    // }
+    onChangeOption([...value, option])
+    console.log(selectedItem)
+  }
+
+  const mappedOption: any[] = value ? value.map((value, index) => (
+      <div className={style.labelButton} onClick={e => e.stopPropagation()}>
+        <div key={index} className={style.selectedOption}>{value.label}</div>
+        <div className={style.removeItem} onClick={e => {
+          removeItem(value.id)
+        }}></div>
+      </div>))
+    : []
+
+  // const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+  //   console.log(e.currentTarget.value)
+  //   // setSelectedItem([...selectedItem])
+  //   onChangeOption && onChangeOption(Number(e.currentTarget.value))
+  // }
 
   return (
     <div className={style.container}>
-      <div className={style.selectedBlock}>
-          <span className={style.selectedItem}>
-        {state.map((value, index) => (<button key={index} className={style.labelButton}>{value.label}</button>))}
-      </span>
+      <div className={style.selectedBlock}
+           onClick={e => setIsOpen(prevState => !prevState)}
+      >
+        <div className={style.selectedItemBlock}>
+          {mappedOption}
+        </div>
         <button
           className={`${style.btn} ${isOpen ? style.btnUp : style.btnDown}`}
-          onClick={() => setIsOpen(prevState => !prevState)}></button>
+          onClick={() => setIsOpen(prevState => !prevState)}>
+        </button>
       </div>
 
       <div className={`${style.selectList} ${isOpen ? style.showOptions : ''}`}>
-        <input className={style.searchInput} type="text" placeholder='&#61442; Search'/>
+
+        <div className={style.searchInputBlock}>
+          <input className={style.searchInput} type="text" placeholder='&#61442; Search'/>
+        </div>
+
         <ul className={`${style.options}`} id="multiSelect">
-          {state.map(state => (<li value={state.value} className={style.option}>
-            <img className={style.flags} src={flag} alt="img"/>
-            {state.label}
-            <input className={style.checkBox} type="checkbox" name="checkbox" value="value"/>
-          </li>))}
+          {options ? options.map((state, index) => (
+            <li
+              key={index}
+              value={state.value}
+              className={style.option}
+            >
+              <img className={style.flags} src={flag} alt="img"/>
+              {state.label}
+              <div className={style.checkBoxBlock}>
+                <input className={style.checkBox}
+                       type="checkbox"
+                       onChange={event => {
+                         selectItem(state)
+                       }}
+                       name="checkbox"
+                       value={state.id}/>
+                <span className={style.checkmark}></span>
+              </div>
+            </li>)) : ''}
         </ul>
       </div>
     </div>
