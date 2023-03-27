731e30e739215d30c3bfbc4033b0f8101236a6aa
EduardaNaida <eduarda.naida@gmail.com>
2023-03-27 14:44:48 +0300

    fix: fix state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import React, { useState } from 'react'
 import './App.css'
 import { MultiSelect, type MultiSelectType } from './components/multiselect/MultiSelect'
-
-const state = [
-  { id: 0, label: 'Русский', icon: '../../../public/icons/flags/ru.svg' },
-  { id: 1, label: 'Английский', icon: '/icons/flags/gb.svg' },
-  { id: 2, label: 'Испанский', icon: '/icons/flags/es.svg' },
-  { id: 3, label: 'Немецкий', icon: '/icons/flags/germanFlag.svg' },
-  { id: 4, label: 'Итальянский', icon: '/icons/flags/it.svg' },
-  { id: 5, label: 'Польский', icon: '/icons/flags/pl.svg' }
-]
+import { state } from './data/state'
 
 const App = (): JSX.Element => {
   const [value, setValue] = useState<MultiSelectType[]>([])
---
+++ src/data/state.tsx
@@ -0,0 +1,15 @@
+import ruFlag from '../icons/flags/ru.svg'
+import ukFlag from '../icons/flags/gb.svg'
+import esFlag from '../icons/flags/es.svg'
+import germanFlag from '../icons/flags/germanFlag.svg'
+import itFlag from '../icons/flags/it.svg'
+import plFlag from '../icons/flags/pl.svg'
+
+export const state = [
+  { id: 0, label: 'Русский', icon: ruFlag },
+  { id: 1, label: 'Английский', icon: ukFlag },
+  { id: 2, label: 'Испанский', icon: esFlag },
+  { id: 3, label: 'Немецкий', icon: germanFlag },
+  { id: 4, label: 'Итальянский', icon: itFlag },
+  { id: 5, label: 'Польский', icon: plFlag }
+]
